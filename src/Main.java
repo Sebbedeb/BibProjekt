44207bf05cae8ddefab4f1fa4c08e4877f02d046
Sebbedeb <zor.seb.0901@gmail.com>
2023-02-20 12:08:22 +0100

    Klar til tirsdagsopgave

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,12 @@
-import Database.ConnectionConfiguration;
 import Entitet.Låner;
+import Mappere.LånerMapper;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.util.*;
+import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) {
-
-        List<Låner> lånerList = new ArrayList<>();
-
-        try {
-            Connection connection = ConnectionConfiguration.getConnection();
-            PreparedStatement statement = connection.prepareStatement("SELECT * FROM bibliotekonsdag.låner");
-
-            ResultSet result = statement.executeQuery();
-
-            while (result.next()) {
-                int id = result.getInt("idLåner");
-                String navn = result.getString("navn");
-                String adresse = result.getString("adresse");
-                int postnr = result.getInt("postnr");
-
-                Låner låner = new Låner(id, navn, adresse, postnr);
-                lånerList.add(låner);
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        for (Låner låner : lånerList) {
-            System.out.println(låner.toString());
-        }
+    public static void main(String[] args)
+    {
+        LånerMapper lånerMapper = new LånerMapper();
+        ArrayList<Låner> lånerList = lånerMapper.getLånerList();
     }
 }
---
+++ src/Mappere/LånerMapper.java
@@ -0,0 +1,43 @@
+package Mappere;
+
+import Database.ConnectionConfiguration;
+import Entitet.Låner;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+public class LånerMapper {
+
+
+    public ArrayList<Låner> getLånerList() {
+
+        ArrayList<Låner> lånerList = new ArrayList<>();
+
+        try {
+            Connection connection = ConnectionConfiguration.getConnection();
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM bibliotekonsdag.låner");
+
+            ResultSet result = statement.executeQuery();
+
+            while (result.next()) {
+                int id = result.getInt("idLåner");
+                String navn = result.getString("navn");
+                String adresse = result.getString("adresse");
+                int postnr = result.getInt("postnr");
+
+                Låner låner = new Låner(id, navn, adresse, postnr);
+                lånerList.add(låner);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        for (Låner låner : lånerList) {
+            System.out.println(låner.toString());
+        }
+        return lånerList;
+    }
+}
