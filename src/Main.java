72d0e986aa0764893d731c795f1951d2543610de
Sebbedeb <zor.seb.0901@gmail.com>
2023-02-20 10:11:15 +0100

    Laver getString

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args)
     {
-
+        System.out.println("Du hedder: " + getString("Hvad hedder du?"));
     }
 
     public static String getString(String s)
